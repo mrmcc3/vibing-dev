54c1589ed59439b2b4cde882b368af4aadf49ec9
Michael McClintock <michael.mcclintock@hey.com>
2022-10-25 15:18:46 +1000

    more radix color scales

--- src/site/styles/tailwind.config.cjs
+++ src/site/styles/tailwind.config.cjs
@@ -1,5 +1,17 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
+function radixColor(name, i) {
+  return `hsl(var(--${name}${i}) / <alpha-value>)`;
+}
+
+function radixScale(name) {
+  const scale = { DEFAULT: radixColor(name, 9) };
+  for (const i of Array(12).keys()) {
+    scale[`${i + 1}`] = radixColor(name, i + 1);
+  }
+  return scale;
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
@@ -10,26 +22,10 @@ module.exports = {
   ],
   theme: {
     colors: {
-      gray: {
-        // Subtle Backgrounds
-        1: "hsl(var(--gray1) / <alpha-value>)",
-        2: "hsl(var(--gray2) / <alpha-value>)",
-        // Component Backgrounds
-        3: "hsl(var(--gray3) / <alpha-value>)", // normal
-        4: "hsl(var(--gray4) / <alpha-value>)", // hover
-        5: "hsl(var(--gray5) / <alpha-value>)", // active
-        // Borders
-        6: "hsl(var(--gray6) / <alpha-value>)", // subtle / non-interactive
-        7: "hsl(var(--gray7) / <alpha-value>)", // interactive / focus rings
-        8: "hsl(var(--gray8) / <alpha-value>)", // hover
-        // Solid Backgrounds
-        DEFAULT: "hsl(var(--gray9) / <alpha-value>)",
-        9: "hsl(var(--gray9) / <alpha-value>)", // purest
-        10: "hsl(var(--gray10) / <alpha-value>)", // Hover
-        // Text
-        11: "hsl(var(--gray11) / <alpha-value>)", // low contrast
-        12: "hsl(var(--gray12) / <alpha-value>)", // high contrast
-      },
+      gray: radixScale("gray"),
+      teal: radixScale("teal"),
+      purple: radixScale("purple"),
+      bronze: radixScale("bronze"),
     },
     extend: {
       fontFamily: {

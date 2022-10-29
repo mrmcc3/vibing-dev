abc8c095290eedc5c147aa7b0d45ee9c807e912f
Michael McClintock <michael.mcclintock@hey.com>
2022-10-29 13:06:14 +1000

    tweaks

--- src/shared/colorscheme/Toggle.svelte
+++ src/shared/colorscheme/Toggle.svelte
@@ -15,7 +15,7 @@
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="currentColor"
-      class="w-7 h-7"
+      class="w-7 h-7 text-[#0091ff] hover:text-[#369eff]"
     >
       <path
         fill-rule="evenodd"
@@ -28,7 +28,7 @@
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="currentColor"
-      class="w-7 h-7"
+      class="w-7 h-7 text-[#ffb224] hover:text-[#ffa01c]"
     >
       <path
         d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z"
@@ -41,7 +41,7 @@
       viewBox="0 0 24 24"
       stroke-width="1.5"
       stroke="currentColor"
-      class="w-7 h-7"
+      class="w-7 h-7 text-[#0091ff] hover:text-[#369eff]"
     >
       <path
         stroke-linecap="round"
@@ -56,7 +56,7 @@
       viewBox="0 0 24 24"
       stroke-width="1.5"
       stroke="currentColor"
-      class="w-7 h-7"
+      class="w-7 h-7 text-[#ffb224] hover:text-[#ffa01c]"
     >
       <path
         stroke-linecap="round"

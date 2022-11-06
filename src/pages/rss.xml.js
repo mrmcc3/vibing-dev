bf50888d0152686b4ea834665df0a251bfd64ba3
Michael McClintock <michael.mcclintock@hey.com>
2022-11-06 15:08:15 +1000

    titles for notes

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,13 +4,18 @@ export function get() {
   const notes = Object.values(
     import.meta.glob("./**/*.md", { eager: true })
   ).filter(({ frontmatter: { draft, pubDate } = {} }) => !draft && pubDate);
+  notes.sort(
+    (a, b) =>
+      new Date(b.frontmatter.pubDate).getTime() -
+      new Date(a.frontmatter.pubDate).getTime()
+  );
   return rss({
     title: "vibing.dev",
     description: "Outlet for whatever I'm vibing. Mostly web dev.",
     site: import.meta.env.SITE,
     items: notes.map(({ url, frontmatter: fm, compiledContent }) => ({
       link: url,
-      title: fm.title || url,
+      title: fm.title || `Note ${url.split("/").at(-1)}`,
       pubDate: fm.pubDate,
       description: fm.description || compiledContent(),
     })),

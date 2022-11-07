76f420088cfdcfe867e0c2e1c206bbc4ae60bedb
Michael McClintock <michael.mcclintock@hey.com>
2022-11-07 21:09:22 +1000

    Blockquote, Paragraph, Pre & Code

--- src/site/mdx/components.ts
+++ src/site/mdx/components.ts
@@ -1,5 +1,11 @@
 import Blockquote from "./Blockquote.astro";
+import Paragraph from "./Paragraph.astro";
+import Preformatted from "./Preformatted.astro";
+import Code from "./Code.astro";
 
 export const components = {
   blockquote: Blockquote,
+  p: Paragraph,
+  pre: Preformatted,
+  code: Code,
 };

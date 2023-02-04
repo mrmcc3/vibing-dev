b0dab520e823392a189a1e5cd50581deb9f449fb
Michael McClintock <mrmcc3@gmail.com>
2023-02-05 07:25:16 +1000

    vitals on idle

---
+++ src/lib/vercel/vitals.svelte
@@ -0,0 +1,50 @@
+<script>
+  import { onCLS, onFCP, onFID, onLCP, onTTFB } from "web-vitals";
+  import { onMount } from "svelte";
+
+  const vitalsUrl = "https://vitals.vercel-analytics.com/v1/vitals";
+
+  function getConnectionSpeed() {
+    return "connection" in navigator &&
+      navigator["connection"] &&
+      "effectiveType" in navigator["connection"]
+      ? navigator["connection"]["effectiveType"]
+      : "";
+  }
+
+  function sendToAnalytics(metric) {
+    const body = {
+      dsn: import.meta.env.VERCEL_ANALYTICS_ID,
+      id: metric.id, // v2-1653884975443-1839479248192
+      href: location.href, // https://my-app.vercel.app/blog/my-test
+      event_name: metric.name, // TTFB
+      value: metric.value.toString(), // 60.20000000298023
+      speed: getConnectionSpeed(), // 4g
+    };
+    const blob = new Blob([new URLSearchParams(body).toString()], {
+      type: "application/x-www-form-urlencoded",
+    });
+    if (navigator.sendBeacon) {
+      navigator.sendBeacon(vitalsUrl, blob);
+    } else {
+      fetch(vitalsUrl, {
+        body: blob,
+        method: "POST",
+        credentials: "omit",
+        keepalive: true,
+      });
+    }
+  }
+
+  onMount(() => {
+    try {
+      onFID(sendToAnalytics);
+      onTTFB(sendToAnalytics);
+      onLCP(sendToAnalytics);
+      onCLS(sendToAnalytics);
+      onFCP(sendToAnalytics);
+    } catch (err) {
+      console.error("[Analytics]", err);
+    }
+  });
+</script>

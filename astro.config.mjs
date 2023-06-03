cd9cd3d606723170b93c0226b2f1246a8eb27267
Michael McClintock <mrmcc3@gmail.com>
2023-06-03 20:17:05 +1000

    try vercel adapter with hybrid output

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import getReadingTime from "reading-time";
 import { toString } from "mdast-util-to-string";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/edge";
 
 export function remarkReadingTime() {
   return function (tree, { data }) {
@@ -35,4 +36,9 @@ export default defineConfig({
     tailwind(),
   ],
   trailingSlash: "never",
+  adapter: vercel(),
+  output: "hybrid",
+  experimental: {
+    hybridOutput: true,
+  },
 });

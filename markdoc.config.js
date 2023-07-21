742997299c90b2822846b3a9ffd6c0fa92c2186e
Michael McClintock <mrmcc3@gmail.com>
2023-07-21 11:52:37 +1000

    prettier v3 astro view transitions

--- markdoc.config.js
+++ markdoc.config.js
@@ -1,9 +1,11 @@
-import { defineMarkdocConfig, nodes } from "@astrojs/markdoc/config";
+import { defineMarkdocConfig, component } from "@astrojs/markdoc/config";
 import shiki from "@astrojs/markdoc/shiki";
-import Box from "components/box.astro";
 
 export default defineMarkdocConfig({
   extends: [shiki({ theme: "css-variables" })],
-  nodes: { document: { ...nodes.document, render: null } },
-  tags: { box: { render: Box } },
+  tags: {
+    box: {
+      render: component("./src/components/box.astro"),
+    },
+  },
 });

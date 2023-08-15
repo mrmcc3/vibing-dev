c316adb5a044236e93415ad44f8edc04e1750b65
Michael McClintock <mrmcc3@gmail.com>
2023-08-15 20:32:30 +1000

    another attempt at transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://vibing.dev",
   trailingSlash: "never",
-  experimental: { assets: true },
+  experimental: { assets: true, viewTransitions: true },
   integrations: [
     svelte(),
     tailwind(),
